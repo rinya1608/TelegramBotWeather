91623f81867b0dc702004260c5530adbc43a2fe1
Renat <rinya1608@mail.ru>
2019-12-13 00:53:38 +0500

    fixed

--- geocoder.py
+++ geocoder.py
@@ -3,11 +3,20 @@ from models import User, Weather, Session, Base, engine
 from decryptions import description
 import requests
 import json
+
 DBSession = Session(bind=engine)
 dataBase = DBSession.query(Weather)
+
 def getPosition(town):
     params = {'geocode':town,'apikey':geoCoderKey, 'format':'json'}
     response = requests.get(urlCoderAPI,params = params)
     data = json.loads(response.text)
-    print(data)
-getPosition('Moсква')
+    point = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
+    return point
+
+def getNameTown(town):
+    params = {'geocode':town,'apikey':geoCoderKey, 'format':'json'}
+    response = requests.get(urlCoderAPI,params = params)
+    data = json.loads(response.text)
+    names = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['formatted'].split(', ')
+    return names[len(names)-1]
